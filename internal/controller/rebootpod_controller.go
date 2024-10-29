068b36f96e6b8dee7021fbf13527ffced3b15917
gauravkr19 <gauravkr19@gmail.com>
2024-10-29 06:06:51 +0000

    refactor code to add goroutine with waitgroup to test TTL == 0

--- internal/controller/rebootpod_controller.go
+++ internal/controller/rebootpod_controller.go
@@ -19,8 +19,10 @@ package controller
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 
 	vault "github.com/hashicorp/vault/api"
@@ -63,98 +65,64 @@ func (r *RebootPodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 	log := log.FromContext(ctx)
 
 	// Fetch the RebootPod CR
-	var rebootPod gauravkr19devv1alpha1.RebootPod
-	if err := r.Get(ctx, req.NamespacedName, &rebootPod); err != nil {
+	// var rebootPod gauravkr19devv1alpha1.RebootPod
+	rebootPod := &gauravkr19devv1alpha1.RebootPod{}
+	if err := r.Get(ctx, req.NamespacedName, rebootPod); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	// Get role from CR spec (mapped to the Vault role for static credentials)
-	jwtRole := rebootPod.Spec.JwtRole
-	vaultEndpointDB := rebootPod.Spec.VaultEndpointDB
-	AuthPath := os.Getenv("AuthPath")
+	wg := &sync.WaitGroup{}
+	var (
+		ttl          int64
+		lastRotation time.Time
+	)
 
-	// jwtToken, err := os.ReadFile("/home/cloud_user/my-controller/unseal/jwt_token")
-	jwtToken, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/token")
-	if err != nil {
-		log.Error(err, "unable to read ServiceAccount JWT token")
-		return ctrl.Result{}, err
-	}
-
-	// JWT login to Vault
-	authData := map[string]interface{}{
-		"jwt":  string(jwtToken),
-		"role": jwtRole,
-	}
-
-	// Authenticate with Vault and set token
-	vaultClient := createVaultClient(ctx)
-	secret, err := vaultClient.Logical().Write(AuthPath, authData)
-	if err != nil || secret.Auth == nil {
-		log.Error(err, "Vault JWT login failed")
-		return ctrl.Result{}, err
-	}
-	// Set the Vault token from the authentication response
-	vaultClient.SetToken(secret.Auth.ClientToken)
-
-	// Query Vault for the specific role's database credentials
-	creds, err := vaultClient.Logical().Read(vaultEndpointDB)
-	if err != nil {
-		log.Error(err, "failed to query Vault for database static credentials")
-		return ctrl.Result{}, err
-	}
-
-	// Extract the ttl and last_vault_rotation
-	ttl, err := strconv.Atoi(creds.Data["ttl"].(json.Number).String())
-	if err != nil {
-		log.Error(err, "failed to convert TTL to integer")
-		return ctrl.Result{}, err
-	}
-	lastRotationStr := creds.Data["last_vault_rotation"].(string)
-	lastRotation, err := time.Parse(time.RFC3339, lastRotationStr)
-	if err != nil {
-		log.Error(err, "failed to parse string time to time.Time")
-	}
-
+	// At the start of your function
+	ttl, lastRotation = r.fetchTTLFromVault(ctx, rebootPod)
 	log.Info("Fetched TTL and rotation info from Vault", "last_vault_rotation", lastRotation, "ttl", ttl)
+
+	// Define a new context and cancel function for the goroutine's lifespan
+	// goroutineCtx, cancel := context.WithCancel(context.Background())
+	// defer cancel() // Ensure the context is canceled when goroutine completes
 
 	// TTL Expiry Check (TTL is 0 or just expired)
 	if ttl == 0 || ttlExpired(lastRotation) {
-		log.Info("TTL is 0, performing rollout restart")
-		err := r.performRolloutRestart(ctx, &rebootPod)
-		if err != nil {
-			log.Error(err, "failed to perform rollout restart")
-			return ctrl.Result{}, err
-		}
-		return ctrl.Result{RequeueAfter: 40 * time.Second}, nil
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err := r.performRolloutRestart(ctx, rebootPod)
+			if err != nil || errors.Is(err, context.Canceled) {
+				log.Error(err, "Error performing rollout restart")
+			}
+		}()
+
+		log.Info("Rollout restart complete", "ttl", ttl)
+		return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
 	}
 
-	// TTL > 1 day: Requeue after 23 hours
-	if ttl > 24*3600 {
-		log.Info("TTL > 24h, requeueing after 23 hours", "ttl", ttl)
-		return ctrl.Result{RequeueAfter: 23 * time.Hour}, nil
+	// Dynamic Requeue Logic
+	var requeueInterval time.Duration
+	switch {
+	case ttl < 3:
+		log.Info("TTL < 3s, requeueing every second", "ttl", ttl)
+		requeueInterval = 1 * time.Second
+	case ttl < 30:
+		log.Info("TTL < 30s, requeueing t/2", "ttl", ttl)
+		requeueInterval = time.Duration(ttl/2) * time.Second
+	case ttl > 24*3600:
+		log.Info("TTL >24h, requeueing after 23h", "ttl", ttl)
+		requeueInterval = 23 * time.Hour
+	default:
+		log.Info("TTL <24h, requeueing t/2", "ttl", ttl)
+		requeueInterval = time.Duration(ttl/2) * time.Second
 	}
 
-	// TTL < 3s: Requeue every second
-	if ttl < 3 {
-		log.Info("TTL < 10s, requeueing every second", "ttl", ttl)
-		return ctrl.Result{RequeueAfter: 1 * time.Second}, nil
-	}
-
-	// TTL < 60s: Requeue after ttl/2
-	if ttl < 60 {
-		requeueInterval := time.Duration(ttl/2) * time.Second
-		log.Info("TTL < 60s, requeueing after ttl/2", "ttl", ttl, "requeueInterval", requeueInterval)
-		return ctrl.Result{RequeueAfter: requeueInterval}, nil
-	}
-
-	// Default case: Requeue based on the remaining TTL (keep checking in chunks)
-	requeueInterval := time.Duration(ttl/2) * time.Second
-	log.Info("Requeueing based on remaining TTL", "ttl", ttl, "requeueInterval", requeueInterval)
+	log.Info("Requeueing based on calculated TTL interval", "ttl", ttl, "requeueInterval", requeueInterval)
 	return ctrl.Result{RequeueAfter: requeueInterval}, nil
 }
 
 // Create a Vault client
-func createVaultClient(ctx context.Context) *vault.Client {
+func (*RebootPodReconciler) fetchTTLFromVault(ctx context.Context, rebootPod *gauravkr19devv1alpha1.RebootPod) (int64, time.Time) {
 	log := log.FromContext(ctx)
 	vaultConfig := vault.DefaultConfig()
 	vaultConfig.Address = os.Getenv("VaultURL")
@@ -166,12 +134,57 @@ func createVaultClient(ctx context.Context) *vault.Client {
 	if err != nil {
 		log.Error(err, "unable to create Vault client")
 	}
-	return vaultClient
+
+	// Get role from CR spec (mapped to the Vault role for static credentials)
+	jwtRole := rebootPod.Spec.JwtRole
+	vaultEndpointDB := rebootPod.Spec.VaultEndpointDB
+	AuthPath := os.Getenv("AuthPath")
+
+	jwtToken, err := os.ReadFile("/home/cloud_user/my-controller/unseal/jwt_token")
+	// jwtToken, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/token")
+	if err != nil {
+		log.Error(err, "unable to read ServiceAccount JWT token")
+	}
+
+	// JWT login to Vault
+	authData := map[string]interface{}{
+		"jwt":  string(jwtToken),
+		"role": jwtRole,
+	}
+
+	// Authenticate with Vault and set token
+	// vaultClient := createVaultClient(ctx)
+	secret, err := vaultClient.Logical().Write(AuthPath, authData)
+	if err != nil || secret.Auth == nil {
+		log.Error(err, "Vault JWT login failed")
+	}
+	// Set the Vault token from the authentication response
+	vaultClient.SetToken(secret.Auth.ClientToken)
+
+	// Query Vault for the specific role's database credentials
+	creds, err := vaultClient.Logical().Read(vaultEndpointDB)
+	if err != nil {
+		log.Error(err, "failed to query Vault for database static credentials")
+	}
+
+	// strconv.ParseInt the ttl string directly as an int64, The 10 specifies the base (decimal), and 64 specifies the bit size (int64). Unlike Atoi which gives int
+	ttl, err := strconv.ParseInt(creds.Data["ttl"].(json.Number).String(), 10, 64)
+	if err != nil {
+		log.Error(err, "failed to convert TTL to integer")
+	}
+	lastRotationStr := creds.Data["last_vault_rotation"].(string)
+	lastRotation, err := time.Parse(time.RFC3339, lastRotationStr)
+	if err != nil {
+		log.Error(err, "failed to parse string time to time.Time")
+	}
+
+	// log.Info("Fetched TTL and rotation info from Vault", "last_vault_rotation", lastRotation, "ttl", ttl)
+	return ttl, lastRotation
 }
 
 // Check if the TTL might have expired based on last known rotation and current time
 func ttlExpired(lastRotation time.Time) bool {
-	// Example: If TTL is expected to expire in the last 30 seconds, consider it expired
+	// TTL expiry missed by 3s, consider it expired
 	expirationBuffer := 3 * time.Second
 	now := time.Now()
 	// Check if the difference between now and the last known rotation time indicates expiration
